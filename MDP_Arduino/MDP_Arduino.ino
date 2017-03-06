645c6936b8fd01ed6ce190a7a00a58e708620820
achhabra95 <aishwarya.chhabra95@gmail.com>
2017-03-06 13:49:10 +0800

    added curved and ultrasonic sensor

--- MDP_Arduino/MDP_Arduino.ino
+++ MDP_Arduino/MDP_Arduino.ino
@@ -1,3 +1,5 @@
+//#include <ultasonic.h>
+
 #include "Encoder.h"
 #include "DualVNH5019MotorShield.h"
 #include "PinChangeInt.h"
@@ -51,7 +53,7 @@ Encoder en(enA1, enB1, enA2, enB2);
 
 SharpIR sensorFR(irFR, 1, 93, SHORT);  // (pin , no.reading b4 calculate mean dist, diff btw 2 consecutive measu taken as valid)
 SharpIR sensorFL(irFL, 1, 93, SHORT);
-SharpIR sensorFM(irFM, 1, 93, SHORT);
+//SharpIR sensorFM(irFM, 1, 93, SHORT);
 SharpIR sensorLM(irLM, 1, 93, SHORT);
 SharpIR sensorLF(irLF, 1, 93, SHORT);
 SharpIR sensorRM(irRM, 1, 93, LONG);
@@ -64,7 +66,7 @@ void setup()
 
   pinMode(irFR, INPUT);
   pinMode(irFL, INPUT);
-  pinMode(irFM, INPUT);
+  //pinMode(irFM, INPUT);
   pinMode(irRM, INPUT);
   pinMode(irLF, INPUT);
   pinMode(irLM, INPUT);
@@ -76,7 +78,7 @@ void setup()
   digitalWrite(enB2, LOW);
   digitalWrite(irFR, LOW);
   digitalWrite(irFL, LOW);
-  digitalWrite(irFM, LOW);
+ // digitalWrite(irFM, LOW);
   digitalWrite(irRM, LOW);
   digitalWrite(irLM, LOW);
   digitalWrite(irLF, LOW);
@@ -109,11 +111,67 @@ void loop()
   //    instructionString = "";
   //    stringReceived = false;
   //  }
-  //  Export_Sensors();
+   Export_Sensors();
   delay(200);
-    forward(100.0);
-//  backward(100.0);
+  //forward(100.0);
+  //curved(40.0);
+ // forward(30.0);
+  // md.setSpeeds(400, 400);
+  //  backward(100.0);
   while (1) {}
+}
+
+
+void wall_alignment()
+{
+  double fl, fm, fr, lm, l_m_distance, m_r_distance, l_r_distance, y1, y2, x;
+  boolean aligned = false, turn_right=false, turn_left=false;
+  int count = 0;
+
+  while (count < 3 && aligned == false)
+  {
+    
+    fl = final_MedianRead(irFL);
+    fm = final_MedianRead(irFM);
+    fr = final_MedianRead(irFR);
+
+    if (fl==fr || fl==fm || fm==fr)
+     aligned =true;
+      
+    if (aligned != false )
+    {
+      if (fl > fr)
+      {
+        y2 = fl - fr;
+        y1 = fm - fr;
+        x = m_r_distance;
+        turn_right = true;
+      }
+      else if (fr < fl)
+      {
+        y2 = fr - fl;
+        y1 = fm - fl;
+        x = l_m_distance;
+        turn_left = true;
+      }
+      
+      double angle_error = (atan2(y2, l_r_distance) + atan2(y1, x)) / 2;
+
+      if (turn_right)
+      {
+        right(angle_error);
+        turn_right = false;
+      }
+      else if (turn_left)
+      {
+        left(angle_error);
+        turn_left = false;
+      }
+      count++;
+    }
+
+   
+  }
 }
 
 
@@ -305,6 +363,120 @@ void right(double angle) {
   md.setBrakes(400, 400);
 }
 
+
+void curved(double forward)
+{
+  double distanceL, distanceR, distanceTraversed, angular_error, v, w, setPoint;
+  distanceTraversed = 0;
+  distanceL = 0;
+  distanceR = 0;
+  angular_error = 0;
+  setPoint = 0;
+  v = 110;
+  w = 0;
+  double Pi = 22 / 7;
+
+  PID PID_angular(&angular_error, &w, &setPoint, angular_kp, angular_ki, angular_kd, DIRECT);
+  PID_angular.SetMode(AUTOMATIC);
+
+  while ( forward - distanceTraversed > 0.5) {
+    distanceL += 2 * Pi * wheelRadius * en.getMotor1Revs();
+    distanceR += 2 * Pi * wheelRadius * en.getMotor2Revs();
+
+    //    Serial.print("   DistanceL: ");
+    //    Serial.print(distanceL/100);
+    //    Serial.print(" ");
+    //    Serial.print("   DistanceR: ");
+    //    Serial.print(distanceR/100);
+    //    Serial.print(" ");
+    distanceTraversed = (distanceL + distanceR) / 2;
+
+    angular_error = distanceL - distanceR;
+    Serial.print("   Forward error: ");
+    Serial.println(angular_error);
+    //Serial.print(" ");
+    //Serial.print("   Distance left: ");
+    //Serial.println(distanceTraversed);
+
+    PID_angular.Compute();
+    if (fabs(forward - distanceTraversed) < forward / 2 && v > 180) {
+      v = v - 0.0005 * v;
+    }
+    else if (v < 350) {
+      v = v + 0.0005 * v;
+    }
+    w = w - 0.0005 * w;
+
+    md.setSpeeds((-v - w) * 11 / 12, v - w);
+
+  }
+
+  v = fabs((-v - w) * 11 / 12) ;
+  double cons_l = v;
+  double  angleTraversed = 0;
+  distanceL = 0;
+  distanceR = 0;
+  setPoint = 0;
+  angular_error = 0;
+  w = 0;
+  md.setSpeeds(-cons_l / 1.5 , cons_l * 2.2 / 1.5);
+
+  while (distanceL < 0.166 * Pi * 3 * distanceBetweenWheels / 2)
+  {
+    distanceL += 2 * Pi * wheelRadius * en.getMotor1Revs();
+    distanceR += 2 * Pi * wheelRadius * en.getMotor2Revs();
+    Serial.print("   distanceL ");
+    Serial.print(distanceL);
+    Serial.print(" ");
+
+  }
+
+
+  md.setBrakes(400, 400);
+
+
+
+  /*  while ( fabs(90 - fabs(angleTraversed)) > 0.8 ) {
+     distanceL += 2 * Pi * wheelRadius * en.getMotor1Revs();
+     distanceR += 2 * Pi * wheelRadius * en.getMotor2Revs();
+
+     angleTraversed = (distanceR - distanceL) / distanceBetweenWheels;
+     angleTraversed = (angleTraversed * 4068) / 71;
+
+     angular_error = distanceL + distanceR;
+
+     //    Serial.print("   L: ");
+     //    Serial.print(distanceL);
+     //    Serial.print("   R: ");
+     //    Serial.print(distanceR);
+     Serial.print("   angle left: ");
+     Serial.print(fabs(90 - fabs(angleTraversed)) / 90);
+     Serial.print(" ");
+     Serial.print("   Left error: ");
+     Serial.print(angular_error);
+     PID_angular.Compute();
+     if (fabs(90 - fabs(angleTraversed)) < 80.0 && v > 100) {
+       v = v - 0.4;
+     }
+     else if (v < 250) {
+       v = v + 0.4;
+     }
+
+     Serial.print(" ");
+     Serial.print("   v: ");
+     Serial.println(v / 125);
+     //    Serial.print("   w: ");
+     //    Serial.println(w);
+
+     md.setSpeeds(cons_l , v + 2*w);
+    }*/
+
+  //  md.setBrakes(400, 400);
+
+
+
+}
+
 void readEncoder1() {
   en.rencoder1();
 }
@@ -347,7 +519,8 @@ int distanceFinder(int pin)
       dis = sensorFL.distance();
       break;
     case irFM:
-      dis = sensorFM.distance();
+      //dis = sensorFM.distance();
+      dis = Ultra_Sensor();
       break;
     case irLF:
       dis = sensorLF.distance();
@@ -422,10 +595,53 @@ void avoidObs() {
     forward(10.0);
   }
 
-forward(10.0);
+  forward(10.0);
   left(90.0);
   forward(10.0);
+
+
+}
+
+void PWM_Mode_Setup()
+{
+  int URPWM = 6; // PWM Output 0-25000US,Every 50US represent 1cm 3
+  int URTRIG = 12; // PWM trigger pin 5  
+  uint8_t EnPwmCmd[4] = {0x44, 0x02, 0xbb, 0x01};
+  pinMode(URTRIG, OUTPUT);                    // A low pull on pin COMP/TRIG
+  digitalWrite(URTRIG, HIGH);                 // Set to HIGH
+
+  pinMode(URPWM, INPUT);                      // Sending Enable PWM mode command
+
+  for (int i = 0; i < 4; i++)
+  {
+    Serial.write(EnPwmCmd[i]);
+  }
+}
+
+double Ultra_Sensor()
+{ // a low pull on pin COMP/TRIG  triggering a sensor reading
+  int URPWM = 6; // PWM Output 0-25000US,Every 50US represent 1cm 3
+  int URTRIG = 12; // PWM trigger pin 5
+  double Distance = 0;
+  digitalWrite(URTRIG, LOW);
+  digitalWrite(URTRIG, HIGH);               // reading Pin PWM will output pulses
+
+  unsigned long DistanceMeasured = pulseIn(URPWM, LOW);
+
+  if (DistanceMeasured >= 10200)
+  { // the reading is invalid.
+    //Serial.println("Invalid");
+  }
+  else
+  {
+    Distance = DistanceMeasured / 50;       // every 50us low level stands for 1cm
+   // Serial.print("Distance=");
+   // Serial.print(Distance);
+   // Serial.println("cm");
+  }
   
+  return Distance;
   
+
 }
 
